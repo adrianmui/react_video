354a7a3b40c640fa4554e6583940c684c009db56
adrian mui <adrian.mui@gmail.com>
2017-06-19 03:10:56 -0500

    took out sample component

--- src/App.js
+++ src/App.js
@@ -3,27 +3,17 @@ import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-const div = React.DOM.div;
-const h1 = React.DOM.h1;
+import sample from './components/sample/sample';
 
-const component1 = React.createClass({
-  render: function() {
-    return (
-      div(null,
-        h1(null, this.props.desc))
-    )
-  }
-});
-
-const factory1 = React.createFactory(component1);
+const factory1 = React.createFactory(sample);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        { factory1({desc: 'first sentence'}) }
-        { factory1({desc: 'second sentence'}) }
-        { factory1({desc: 'third sentence'}) }
+        { factory1({desc: 'first sentence', color: 'red'}) }
+        { factory1({desc: 'second sentence', color: 'blue'}) }
+        { factory1({desc: 'third sentence', color: 'green'}) }
       </div>
     );
   }
