0f06976aebde588998093141f2d80c7d884a8c51
adrian mui <adrian.mui@gmail.com>
2017-06-30 00:51:06 -0500

    added filter search and switched to stateful class

--- src/search.js
+++ src/search.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import ShowCards from './showCards/showCards'
@@ -9,20 +9,43 @@ const jsonStyle = {
   textAlign: 'left'
 }
 
-const Search = () => {
-  return (
+class Search extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchTerm: `debug dis`
+    }
+
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
+  }
+
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+  
+  render() { 
+    return (
     <div style={ jsonStyle }>
       <Link to='/'>Home</Link>
+      
+      <header>
+        <h1> {this.state.searchTerm} </h1>
+        <input type="text" onChange={this.handleSearchTermChange} value={this.state.searchTerm} placeholder="Search"/>
+      </header>
+      
       <ul>
           {
-            staticData.shows.map( show => (
+            staticData.shows
+              .filter( show => `${show.title} ${show.description}`.toUpperCase().includes(this.state.searchTerm.toUpperCase()))
+              .map( show => (
                 <ShowCards show={show}/>
               )
             )
           }
       </ul>
     </div>
-  )
+    )
+  }
 }
 
 export default Search;
